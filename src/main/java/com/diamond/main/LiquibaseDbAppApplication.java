a5310b771cddc5eaa381f74456c3cfe23ff9eb5d
ajaycheke96 <77390789+ajaycheke96@users.noreply.github.com>
2022-02-10 10:42:59 +0530

    first commit

---
+++ src/main/java/com/diamond/main/LiquibaseDbAppApplication.java
@@ -0,0 +1,13 @@
+package com.diamond.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseDbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseDbAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diamond/main/LiquibaseDbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diamond.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseDbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
